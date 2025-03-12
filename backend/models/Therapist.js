bd7e0df92311087154e7ef4cbeb1953e56f1e617
kalvium <kalvium@kalvium.community>
2025-03-12 15:14:44 +0530

    ptiend ID modify

--- backend/models/Therapist.js
+++ backend/models/Therapist.js
@@ -1,10 +1,26 @@
 const mongoose = require("mongoose");
+const bcrypt=require("bcryptjs");
 
 const TherapistSchema = new mongoose.Schema({       //therapist/audiologist schema
   name: { type: String, required: true },
   designation: { type: String, required: true },
   hospital: { type: String, required: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true } //keep this hashed for security
+});
+
+//Hash password before saving
+
+TherapistSchema.pre("save", async function (next) {
+  if(!this.isModified("password")) return next();
+  try{
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+
+  }catch(err){
+    next(err);
+  }
+ 
 });
 
 module.exports = mongoose.model("Therapist", TherapistSchema);
