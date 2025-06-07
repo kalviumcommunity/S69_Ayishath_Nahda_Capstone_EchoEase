6babfbc8f9db8ff135309bab1053b72bbc13fbc7
AyishathNahda <ayishathnahda.9i@gmail.com>
2025-06-07 23:07:36 +0530

    Update Profile.jsx

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -93,7 +93,7 @@ const Profile = () => {
       }
 
       const res = await axios.put(
-        "${import.meta.env.VITE_META_URI}/api/therapist/profile", 
+        `${import.meta.env.VITE_META_URI}/api/therapist/profile`, 
         formData, 
         {
           headers: {
